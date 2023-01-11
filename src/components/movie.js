befa80fc2a81771d933fa0560a25a365152143ce
rijfe <kyw42510@naver.com>
2023-01-11 17:44:37 +0900

    detail start

--- src/components/movie.js
+++ src/components/movie.js
@@ -2,20 +2,14 @@ import PropType from "prop-types";
 import { Link } from "react-router-dom";
 import styles from "../Button.module.css";
 
-function Movie({ coverImg, title, year, rating, id }) {
-
-
+function Movie({ coverImg, title, year, id }) {
     return (
-        
         <div className={styles.container}>
             <fieldset className={styles.border}>
                 <legend className={styles.title}>{title}({year})</legend>
                 <Link to={`/movie/${id}`}>
                     <img src={coverImg} alt={title} style={{marginTop:"15px"}} />
                 </Link>
-                <div className={styles.ratingBox}>
-                    <h2 className={styles.rating}>{rating}/10</h2>
-                </div>
             </fieldset>
         </div>
     );
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,23 +1,36 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-function Detail(){
-    const {id} = useParams();
-    
-    const getDetail = async() =>{
+import styles from "../Detail.module.css";
+
+function Detail() {
+    const { id } = useParams();
+    const [movie, setMovie] = useState([]);
+
+
+    const getDetail = async () => {
         const json = await (
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
-        console.log(json);
+        console.log(json.data.movie);
+        setMovie(json.data.movie);
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         getDetail();
-    },[]);
+    }, []);
 
-    return(
-        <div>
-            <h1>Detail</h1>
+    return (
+        <div style={{
+            backgroundImage:`url(${movie.background_image})`,
+        }}>
+            <div className={styles.container}>
+                <img src={movie.medium_cover_image}/>
+                <ul>
+                    {movie?.genres.map((data)=>{return <li style={{color:'white'}}>{data}</li>})}
+                </ul>
+                <h1>{movie.title}({movie.year})</h1>
+            </div>
         </div>
     );
 }
